7207271dfb02d5af04244051b74a8df24f4c7a98
superfroggman <49985901+superfroggman@users.noreply.github.com>
2021-01-26 17:27:45 +0100

    added some cool effects

--- main.lua
+++ main.lua
@@ -6,7 +6,28 @@ function update()
     
     if timepassed > 600 then
         timepassed = 0
-        
-        SetPlayerHealth(0.1)
+		
     end
 end
+
+
+
+function lowHealth()
+	SetPlayerHealth(0.1)
+end
+
+function launchPlayerUp()
+	SetPlayerVelocity(Vec(0, 30, 0))
+end
+
+function pauseGame()
+	SetPaused(true)
+end
+
+function launchVehicleUp()
+	local vehicle = GetPlayerVehicle()
+	if vehicle ~= 0 then
+		SetBodyVelocity(GetVehicleBody(vehicle), Vec(0,30,0))
+	end
+end
+
