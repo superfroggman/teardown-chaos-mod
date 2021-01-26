ed705b6151e35d4d74939a6682449f0fcffb4f42
Linus Romland <romlandlinus@gmail.com>
2021-01-26 17:34:17 +0100

    fixed UI that broke in merge

--- main.lua
+++ main.lua
@@ -1,16 +1,20 @@
 
 timepassed = 0
+timeToChaos = 600;
+latestChaos = "";
 
 function update()
 	timepassed = timepassed + 1
     
-    if timepassed > 600 then
+    if timepassed > 250 then
+        latestChaos = ""
+    end
+
+    if timepassed > timeToChaos  then
         timepassed = 0
-		
+		--run random function here
     end
 end
-
-
 
 function lowHealth()
 	SetPlayerHealth(0.1)
@@ -31,3 +35,14 @@ function launchVehicleUp()
 	end
 end
 
+function draw()
+    UiTextOutline(0,0,0,1, 0.5)
+    UiFont("bold.ttf", 45)
+    UiAlign("top left")
+    UiText(tostring(round((timeToChaos/60) - (timepassed/60))), true)
+    UiText(latestChaos, true)
+end
+
+function round(x)
+    return x>=0 and math.floor(x+0.5) or math.ceil(x-0.5)
+  end
