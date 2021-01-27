a79c598177f8c307ac01136e3bd142ad127a0c8f
Linus Romland <romlandlinus@gmail.com>
2021-01-27 19:14:27 +0100

    add increase and decrease in options for timeToChaos

--- options.lua
+++ options.lua
@@ -1,15 +1,36 @@
+timeToChaos = GetInt("savegame.mod.timeToChaos", 10)
+
 function draw()
 	UiTranslate(UiCenter(), 250)
 	UiAlign("center middle")
-
+	if timeToChaos == 0 then
+		timeToChaos = 10
+	end
+	SetInt("savegame.mod.timeToChaos", timeToChaos)
 	--Title
 	UiFont("bold.ttf", 48)
 	UiText("Chaos Mod options")
 	
 	--Draw buttons
-	UiTranslate(0, 200)
+	UiTranslate(0, 100)
 	UiFont("regular.ttf", 26)
-	value = UiSlider("images/dot.png", "x", value, 0, 100)
+	UiText("Time between chaos: "..timeToChaos)
+	UiTranslate(0, 50)
+	UiButtonImageBox("ui/common/box-outline-6.png", 6, 6)
+	UiPush()
+		UiTranslate(-110, 0)
+		if UiTextButton("Decrease", 200, 40) then
+			if timeToChaos > 1 then
+				timeToChaos = timeToChaos - 1
+			end
+		end
+		UiTranslate(220, 0)
+		if UiTextButton("Increase", 200, 40) then
+			if timeToChaos < 100 then
+				timeToChaos = timeToChaos + 1
+			end
+		end
+	UiPop()
 	
 	UiTranslate(0, 100)
 	if UiTextButton("Close", 200, 40) then
