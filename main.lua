13dcec65b2cabc9166701ffa1e8ee96704a3907b
superfroggman <49985901+superfroggman@users.noreply.github.com>
2021-02-05 10:40:48 +0100

    fixed active effects and added boost effect

--- main.lua
+++ main.lua
@@ -6,7 +6,7 @@ activeEffects = {}
 
 function init()
 	if timeToChaos == 0 then
-		timeToChaos = 10
+		timeToChaos = 600
 	end
 end
 
@@ -21,7 +21,7 @@ function update()
 		end
 
 		--Decrease time left
-		effect[3] = effect[3] - 1
+		activeEffects[i][3] = effect[3] - 1
 		--Remove active effect if time has run out
 		if(effect[3] <= 0) then
 			table.remove(activeEffects, i)
@@ -90,6 +90,41 @@ function knock()
     PlaySound(LoadSound("./sound/knock.ogg"))
 end
 
+--TAKEN DIRECTLY FROM DEV EXAMPLE
+power = 0.5
+function vehicleBoost()
+	--Only active when player is in vehicle
+	v = GetPlayerVehicle()
+	if v > 0 then
+		--Compute tail point and back direction on vehicle in world space
+		local t = GetVehicleTransform(v)
+		local p = TransformToParentPoint(t, Vec(0, 0.5, 3))
+		local d = TransformToParentVec(t, Vec(0, 0, 1))
+
+		if InputDown("up") then
+			--Push the vehicle forwards
+			local b = GetVehicleBody(v)	
+			local v = GetBodyVelocity(b)
+			v = VecAdd(v, VecScale(d, -power))
+			SetBodyVelocity(b, v)
+
+			--Engaged particle effects
+			local pvel = VecScale(v, 0.7)
+			SpawnParticle("fire", p, VecAdd(pvel, VecScale(d, 2)), 1.0, 0.5)
+			SpawnParticle("smoke", p, VecAdd(pvel, VecScale(d, 5)), 1, 3.0)
+			SpawnParticle("darksmoke", p, VecAdd(pvel, VecScale(d, 10)), 1.5, 1.0)
+			
+			--Play sound
+			PlayLoop(LoadLoop("rocket.ogg"), t.pos, 0.75)
+		else
+			--Idle particle effect
+			SpawnParticle("fire", p, VecScale(d, 1), 0.3, 0.1)
+			SpawnParticle("smoke", p, VecScale(d, 2), 0.5, 1.0)
+		end
+	end
+end
+
+
 
 
 --EFFECT LIST
@@ -102,7 +137,8 @@ effects =
 	{hole, "Diggy Diggy Hole", 0},
 	{removeVehicle, "Bye Bye Vehicle", 0},
     {fireTrail, "Fire go brrrrrr", 300},
-    {knock, "Who's there", 0}
+    {knock, "Who's there?", 0},
+	{vehicleBoost, "BOOST", 300}
 }
 
 function runRandomFunction ()
@@ -122,7 +158,12 @@ function runRandomFunction ()
 		if(effect[3] == 0) then
 			func()
 		else
-			activeEffects[#activeEffects + 1] = effect
+			--Need to do this to pass by value and not by reference
+			--Can probably be done better
+			qa = effect[1]
+			qb = effect[2]
+			qc = effect[3]
+			activeEffects[#activeEffects + 1] = {qa, qb, qc}
 		end
 		--Set text to effect text
 		latestChaos = effect[2]
